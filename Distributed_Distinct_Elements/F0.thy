ecb68d7e2d3dc1ff7a519b73f0a11af9d152d53f
Emin Karayel <me@eminkarayel.de>
2022-11-23 02:10:22 +0100

    History independence + Framework

--- Distributed_Distinct_Elements/F0.thy
+++ Distributed_Distinct_Elements/F0.thy
@@ -5,25 +5,55 @@ theory F0
     "Pseudorandom_Combinators"
 begin
 
-definition C3 :: int where "C3 = 17"
-definition C6 :: nat where "C6 = 17"
+definition C2 :: real where "C2 = 3^2*2^22"
+definition C3 :: int where "C3 = 34"
+definition C6 :: nat where "C6 = 2^5"
 
 context
   fixes n :: nat
   fixes \<epsilon> :: rat
   fixes \<delta> :: rat
   assumes n_gt_0: "n > 0" 
+  assumes \<epsilon>_gt_0: "\<epsilon> > 0" and \<epsilon>_lt_1: "\<epsilon> < 1"
+  assumes \<delta>_gt_0: "\<delta> > 0" and \<delta>_lt_1: "\<delta> < 1"
 begin
 
-definition b :: nat where "b = nat (\<lceil>1/ \<delta>\<^sup>2\<rceil>)"
+definition b_base :: nat where "b_base = nat (\<lceil>log 2 (C2 / (of_rat \<delta>)^2)\<rceil>)"
+definition b :: nat where "b = 2^b_base"
 definition l :: nat where "l = nat (\<lceil>ln (1/real_of_rat \<epsilon>)\<rceil>)"
 definition k :: nat where "k = nat (\<lceil>ln (b\<^sup>2)\<rceil>)"
 
+lemma powr_less_1: "x > 0 \<Longrightarrow> x < 1 \<Longrightarrow> (x::real)^2 < 1"
+  using power_strict_decreasing
+  by (metis pos2 power_0)
+
+lemma k_gt_0: "k > 0" sorry
+
 lemma l_gt_0: "l > 0" sorry
-lemma b_gt_0: "b > 0" sorry
+lemma b_base_ge_26: "b_base \<ge> 26"
+proof -
+  have "2 powr 25 < C2" unfolding C2_def by simp
+  also have "... < (C2*1) / (real_of_rat \<delta>)\<^sup>2"
+    using \<delta>_gt_0 \<delta>_lt_1 unfolding C2_def
+    by (intro iffD2[OF pos_less_divide_eq] zero_less_power mult_strict_left_mono powr_less_1) auto
+  finally have "2 powr 25 < C2 / (real_of_rat \<delta>)\<^sup>2" by simp
+  hence "log 2 (C2 / (real_of_rat \<delta>)\<^sup>2) > 25" 
+    using \<delta>_gt_0 unfolding C2_def
+    by (intro iffD2[OF less_log_iff] divide_pos_pos zero_less_power) auto
+  hence "\<lceil>log 2 (C2 / (real_of_rat \<delta>)\<^sup>2)\<rceil> \<ge> 26" by simp
+  thus ?thesis
+    unfolding b_base_def by linarith
+qed
+
+lemma b_min: "b \<ge> 2^26"
+  unfolding b_def
+  by (meson b_base_ge_26  nat_power_less_imp_less not_less power_eq_0_iff power_zero_numeral)
+
+definition \<Psi> where 
+  "\<Psi> = (\<G> 2 n) \<times>\<^sub>S (\<H> 2 n [(C6*b\<^sup>2)]\<^sub>S) \<times>\<^sub>S (\<H> k (C6*b\<^sup>2) [b]\<^sub>S)"
 
 definition \<Omega> where 
-  "\<Omega> = \<E> ((\<G> 2 n) \<times>\<^sub>S (\<H> 2 n (C6*b\<^sup>2)) \<times>\<^sub>S (\<H> k (C6*b\<^sup>2) b)) l 0.1"
+  "\<Omega> = \<E> l 0.1 \<Psi>"
 
 type_synonym f0_state = "(nat \<Rightarrow> nat \<Rightarrow> int) \<times> (nat)"
 
@@ -124,72 +154,151 @@ lemma \<tau>\<^sub>2_step: "\<tau>\<^sub>2 \<omega> A (x+y) = (\<lambda>i j. max
 lemma \<tau>\<^sub>3_step: "compress_step (\<tau>\<^sub>3 \<omega> A x) = \<tau>\<^sub>3 \<omega> A (x+1)"
   using \<tau>\<^sub>2_step[where y="1"] by (simp del:\<tau>\<^sub>2.simps)
 
+lemma prod_right_mono: "B \<subseteq> C \<Longrightarrow> A \<times> B \<subseteq> A \<times> C"
+  by auto
 
-(*
-1011    0     1/2
-0101    1     1/4
-0011    2     1/8
-0001    3     1/16
-0000    4     1/16
-*)
-
-lemma max_s': "\<tau>\<^sub>2 \<omega> A (nat \<lceil>log 2 n\<rceil>+1) i j = (-1)"
+lemma sample_set_\<Psi>:
+  "sample_set \<Psi> \<subseteq> sample_set (\<G> 2 n) \<times> sample_set (\<H> 2 n [(C6*b\<^sup>2)]\<^sub>S) \<times> sample_set (\<H> k (C6*b\<^sup>2) [b]\<^sub>S)" (is "_ \<subseteq> ?R")
 proof -
-  obtain f g h where w_i: "select \<Omega> \<omega> i  = (f,g,h)" 
+  have "sample_set \<Psi> \<subseteq> sample_set (\<G> 2 n) \<times> sample_set (\<H> 2 n [(C6*b\<^sup>2)]\<^sub>S \<times>\<^sub>S (\<H> k (C6*b\<^sup>2) [b]\<^sub>S))"
+    unfolding \<Psi>_def by (intro prod_sample_set)
+  also have "... \<subseteq> ?R"
+    by (intro prod_right_mono prod_sample_set)
+  finally show ?thesis
+    by simp
+qed
+
+lemma f_range: 
+  assumes "(f,g,h) \<in> sample_set \<Psi>"
+  shows "f x \<le> max (nat \<lceil>log 2 n\<rceil>) 1"
+proof -
+  have "f \<in> sample_set (\<G> 2 n)"
+    using sample_set_\<Psi> assms by auto
+  then obtain i where f_def:"f = select (\<G> 2 n) i" unfolding sample_set_def by auto
+  show ?thesis
+    unfolding f_def
+    by (intro \<G>_range n_gt_0) auto
+qed
+
+lemma fin_f:
+  assumes "(f,g,h) \<in> sample_set \<Psi>"
+  shows "finite { int (f a) | a. P a }" (is "finite ?M")
+proof -
+  have "finite (range f)"
+    using f_range[OF assms] finite_nat_set_iff_bounded_le by auto
+  hence "finite (range (int \<circ> f))"
+    by (simp add:image_image[symmetric])
+  moreover have "?M \<subseteq> (range (int \<circ> f))"
+    using image_mono by (auto simp add: setcompr_eq_image)    
+  ultimately show ?thesis
+    using finite_subset by auto
+qed
+
+lemma Max_int_range: "x \<le> (y::int) \<Longrightarrow> Max {x..y} = y"
+  by auto
+
+lemma family_1_conditions: "0 < (2::nat)" "0 < n"
+  using n_gt_0 by auto
+
+lemma family_2_conditions:
+  shows "is_prime_power (size [(C6*b\<^sup>2)]\<^sub>S)" "(2::nat) > 0"
+proof -
+  have a:"C6 * b^2 = (2^(5 + b_base*2))" 
+    unfolding C6_def b_def by (simp add: power_mult power_add) 
+  have "is_prime_power (C6 * b\<^sup>2)" 
+    unfolding a by (intro is_prime_power) auto
+  thus "is_prime_power (size [C6 * b\<^sup>2]\<^sub>S)"
+    unfolding nat_sample_space_def by simp
+  show "(2::nat) > 0" by simp
+qed
+
+lemma family_3_conditions:
+  shows "is_prime_power (size [b]\<^sub>S)" "k > 0"
+proof -
+  have "is_prime_power b" 
+    unfolding b_def using b_base_ge_26 by (intro is_prime_power) auto
+  thus "is_prime_power (sample_space.size [b]\<^sub>S)"
+    unfolding nat_sample_space_def by simp
+  show "k > 0" using k_gt_0 by simp
+qed
+
+lemma select_\<Omega>_range: "select \<Omega> \<omega> i \<in> sample_set \<Psi>"
+proof -
+  have "size (\<G> 2 n) > 0" 
+    using \<G>_size[OF family_1_conditions] by simp
+  moreover have "size  (\<H> 2 n [(C6*b\<^sup>2)]\<^sub>S) > 0" 
+    using \<H>_size[OF family_2_conditions] by simp
+  moreover have "size (\<H> k (C6*b\<^sup>2) [b]\<^sub>S) > 0"
+    using \<H>_size[OF family_3_conditions] by simp
+  ultimately have "size \<Psi> > 0" 
+    unfolding \<Psi>_def by (simp add:prod_sample_space_def) 
+
+  thus ?thesis
+    unfolding \<Omega>_def by (intro \<E>_range) auto
+qed
+
+lemma max_s': "\<tau>\<^sub>2 \<omega> A (nat \<lceil>log 2 n\<rceil>+2) i j = (-1)"
+proof -
+  obtain f g h where w_i: "select \<Omega> \<omega> i = (f,g,h)" 
     by (metis prod_cases3)
 
-  have "(- 1) < log 2 (real n) " sorry
-  have "\<tau>\<^sub>0 (select \<Omega> \<omega> i) A j \<le> \<lceil>log 2 (real n)\<rceil>"
-    unfolding w_i \<tau>\<^sub>0.simps
-    apply (intro iffD2[OF Max_le_iff])
-      defer
-      apply simp
-    apply auto
-    defer
-    sorry
+  let ?max_s = "max \<lceil>log 2 (real n)\<rceil> 1"
+
+  have c: "(f,g,h) \<in> sample_set \<Psi>" 
+    using w_i select_\<Omega>_range by metis
+  have a:"int (f x) \<le> ?max_s" for x 
+  proof -
+    have "int (f x) \<le> int (max (nat \<lceil>log 2 n\<rceil>) 1)" 
+      by (intro of_nat_mono f_range[OF c])
+    also have "... = ?max_s" by simp
+    finally show ?thesis by simp
+  qed
+  have "\<tau>\<^sub>0 (select \<Omega> \<omega> i) A j \<le> Max {(-1)..?max_s}"
+    unfolding w_i \<tau>\<^sub>0.simps using a by (intro Max_mono)  auto
+  also have "... = ?max_s" 
+    by (intro Max_int_range) auto
+  finally have "\<tau>\<^sub>0 (select \<Omega> \<omega> i) A j \<le> ?max_s" by simp
   thus ?thesis
     unfolding \<tau>\<^sub>2.simps \<tau>\<^sub>1.simps
     by (intro max_absorb2) linarith
 qed
 
-lemma max_s: "\<not> (is_too_large (\<tau>\<^sub>2 \<omega> A (nat \<lceil>log 2 n\<rceil>+1)))"
+lemma max_s: "\<not> (is_too_large (\<tau>\<^sub>2 \<omega> A (nat \<lceil>log 2 n\<rceil>+2)))"
   using max_s' by (simp add:C3_def mult_less_0_iff del:\<tau>\<^sub>2.simps)
 
 lemma max_mono: "x \<le> (y::int) \<Longrightarrow> max x z \<le> max y z"
   by simp
 
-
 lemma \<tau>\<^sub>0_mono: 
-  assumes "B \<subseteq> {..<n}"
+  assumes "\<psi> \<in> sample_set \<Psi>"
   assumes "A \<subseteq> B"
-  shows "\<tau>\<^sub>0 \<omega> A j \<le> \<tau>\<^sub>0 \<omega> B j"
+  shows "\<tau>\<^sub>0 \<psi> A j \<le> \<tau>\<^sub>0 \<psi> B j"
 proof -
-  obtain f g h where w_i: "\<omega> = (f,g,h)" 
+  obtain f g h where w_i: "\<psi> = (f,g,h)" 
     by (metis prod_cases3)
   show ?thesis
-    unfolding \<tau>\<^sub>0.simps w_i using assms
-    by (intro Max_mono subsetI) (auto simp add:finite_subset) 
+    using assms fin_f unfolding \<tau>\<^sub>0.simps w_i 
+    by (intro Max_mono) auto 
 qed
 
 lemma \<tau>\<^sub>2_mono: 
-  assumes "B \<subseteq> {..<n}"
   assumes "A \<subseteq> B"
   shows "\<tau>\<^sub>2 \<omega> A x i j \<le> \<tau>\<^sub>2 \<omega> B x i j"
-  unfolding \<tau>\<^sub>2.simps \<tau>\<^sub>1.simps
+  unfolding \<tau>\<^sub>2.simps \<tau>\<^sub>1.simps using select_\<Omega>_range
   by (intro max_mono diff_mono \<tau>\<^sub>0_mono assms) auto
 
 lemma is_too_large_antimono: 
-  assumes "B \<subseteq> {..<n}" "A \<subseteq> B"
+  assumes  "A \<subseteq> B"
   assumes "is_too_large (\<tau>\<^sub>2 \<omega> A x)" 
   shows "is_too_large (\<tau>\<^sub>2 \<omega> B x)"
 proof -
   have "C3 * b * l < (\<Sum> (i,j) \<in> {..<l} \<times> {..<b}.  \<lfloor>log 2 (max (\<tau>\<^sub>2 \<omega> A x i j) (-1) + 2)\<rfloor>)"
-    using assms(3) by simp
+    using assms(2) by simp
   also have "... = (\<Sum> y \<in> {..<l} \<times> {..<b}.  \<lfloor>log 2 (max (\<tau>\<^sub>2 \<omega> A x (fst y) (snd y)) (-1) + 2)\<rfloor>)" 
     by (simp add:case_prod_beta del:\<tau>\<^sub>2.simps) 
   also have "... \<le> (\<Sum> y \<in> {..<l} \<times> {..<b}.  \<lfloor>log 2 (max (\<tau>\<^sub>2 \<omega> B x (fst y) (snd y)) (-1) + 2)\<rfloor>)" 
     by (intro sum_mono floor_mono iffD2[OF log_le_cancel_iff] iffD2[OF of_int_le_iff] 
-        add_mono max_mono \<tau>\<^sub>2_mono[OF assms(1,2)]) auto
+        add_mono max_mono \<tau>\<^sub>2_mono[OF assms(1)]) auto
   also have "... = (\<Sum> (i,j) \<in> {..<l} \<times> {..<b}.  \<lfloor>log 2 (max (\<tau>\<^sub>2 \<omega> B x i j) (-1) + 2)\<rfloor>)" 
     by (simp add:case_prod_beta del:\<tau>\<^sub>2.simps) 
   finally have "(\<Sum> (i,j) \<in> {..<l} \<times> {..<b}.  \<lfloor>log 2 (max (\<tau>\<^sub>2 \<omega> B x i j) (-1) + 2)\<rfloor>) > C3 * b * l"
@@ -202,7 +311,7 @@ lemma s_compact: "\<not> (is_too_large (\<tau>\<^sub>2 \<omega> A (s \<omega> A)
   by (intro wellorder_Least_lemma(1)) blast
 
 lemma s_mono: 
-  assumes "B \<subseteq> {..<n}" "A \<subseteq> B"
+  assumes "A \<subseteq> B"
   shows "s \<omega> A \<le> s \<omega> B"
 proof -
   have "\<not> (is_too_large (\<tau>\<^sub>2 \<omega> A (s \<omega> B)))" 
@@ -256,37 +365,35 @@ proof -
 qed
 
 lemma \<tau>\<^sub>0_merge:
-  assumes "A \<subseteq> {..<n}" "B \<subseteq> {..<n}"
+  assumes "(f,g,h) \<in> sample_set \<Psi>"
   shows "\<tau>\<^sub>0 (f,g,h) (A \<union> B) j = max (\<tau>\<^sub>0 (f,g,h) A j) (\<tau>\<^sub>0 (f,g,h) B j)" (is "?L = ?R")
 proof-
   let ?f = "\<lambda>a. int (f a)" 
-  have "?L = Max (?f ` { a . a \<in> (A \<union> B) \<and> h (g a) = j } \<union> {-1})"
-    by (simp add: setcompr_eq_image)
-  also have "... = Max ((?f ` { a . a \<in> A \<and> h (g a) = j } \<union> {-1}) \<union>
-                        (?f ` { a . a \<in> B \<and> h (g a) = j } \<union> {-1}))"
-    by (intro arg_cong[where f="Max"]) auto
-  also have "... = max (Max (?f ` { a . a \<in> A \<and> h (g a) = j } \<union> {-1})) 
-                       (Max (?f ` { a . a \<in> B \<and> h (g a) = j } \<union> {-1}))"
-    using assms finite_nat_iff_bounded
-    by (intro Max_Un finite_UnI finite_imageI) auto
+  have "?L = Max (({ int (f a) |  a . a \<in> A \<and> h (g a) = j } \<union> {-1}) \<union>
+                  ({ int (f a) | a . a \<in> B \<and> h (g a) = j } \<union> {-1}))"
+    unfolding \<tau>\<^sub>0.simps
+    by (intro arg_cong[where f="Max"])  auto
+  also have "... = max (Max ({ int (f a) | a . a \<in> A \<and> h (g a) = j } \<union> {-1})) 
+                       (Max ({ int (f a) | a . a \<in> B \<and> h (g a) = j } \<union> {-1}))"
+    by (intro Max_Un finite_UnI fin_f[OF assms]) auto 
   also have "... = ?R"
-    by (simp add: setcompr_eq_image)
+    by (simp)
   finally show ?thesis by simp
 qed
 
 lemma \<tau>\<^sub>2_merge:
-  assumes "A \<subseteq> {..<n}" "B \<subseteq> {..<n}"
-  shows "\<tau>\<^sub>2 \<omega> (A \<union> B) x i j = max (\<tau>\<^sub>2 \<omega> A x i j) (\<tau>\<^sub>2 \<omega> B x i j)"
+  "\<tau>\<^sub>2 \<omega> (A \<union> B) x i j = max (\<tau>\<^sub>2 \<omega> A x i j) (\<tau>\<^sub>2 \<omega> B x i j)"
 proof -
   obtain f g h where w_i: "select \<Omega> \<omega> i = (f,g,h)" 
     by (metis prod_cases3)
+  have a:"(f,g,h) \<in> sample_set \<Psi>"
+    using select_\<Omega>_range w_i by metis
   show ?thesis
-    by (simp add:w_i \<tau>\<^sub>0_merge[OF assms] del:\<tau>\<^sub>0.simps)
+    by (simp add:w_i \<tau>\<^sub>0_merge[OF a] del:\<tau>\<^sub>0.simps)
 qed
 
 lemma merge1_result:
-  assumes "A \<subseteq> {..<n}" "B \<subseteq> {..<n}"
-  shows "merge1 (\<tau> \<omega> A) (\<tau> \<omega> B) = \<tau>\<^sub>3 \<omega> (A \<union> B) (max (s \<omega> A) (s \<omega> B))"
+   "merge1 (\<tau> \<omega> A) (\<tau> \<omega> B) = \<tau>\<^sub>3 \<omega> (A \<union> B) (max (s \<omega> A) (s \<omega> B))"
 proof -
   let ?smax = "max (s \<omega> A) (s \<omega> B)"
   obtain u where u_def: "s \<omega> A + u = ?smax" 
@@ -299,7 +406,7 @@ proof -
     by (auto simp del:s.simps)
 
   have "\<tau>\<^sub>2 \<omega> (A \<union> B) ?smax = (\<lambda> i j.  max (\<tau>\<^sub>2 \<omega> A ?smax i j) (\<tau>\<^sub>2 \<omega> B ?smax i j))"
-    using \<tau>\<^sub>2_merge[OF assms] by (intro ext)
+    using \<tau>\<^sub>2_merge by (intro ext)
   also have "... = (\<lambda> i j.  max (\<tau>\<^sub>2 \<omega> A (s \<omega> A + u) i j) (\<tau>\<^sub>2 \<omega> B (s \<omega> B + v) i j))"
     unfolding u_def v_def by blast
   also have "... = (\<lambda> i j.  max (max (\<tau>\<^sub>2 \<omega> A (s \<omega> A) i j - u) (-1)) (max (\<tau>\<^sub>2 \<omega> B (s \<omega> B) i j - v) (-1)))"
@@ -319,21 +426,19 @@ proof -
 qed
 
 lemma merge_result:
-  assumes "A \<subseteq> {..<n}" "B \<subseteq> {..<n}"
-  shows "merge (\<tau> \<omega> A) (\<tau> \<omega> B) = \<tau> \<omega> (A \<union> B)" (is "?L = ?R")
+  "merge (\<tau> \<omega> A) (\<tau> \<omega> B) = \<tau> \<omega> (A \<union> B)" (is "?L = ?R")
 proof -
   have a:"max (local.s \<omega> A) (local.s \<omega> B) \<le> local.s \<omega> (A \<union> B)" 
-    using s_mono assms by (simp del:s.simps)
+    using s_mono by (simp del:s.simps)
 
   have "?L = compress (merge1 (\<tau> \<omega> A) (\<tau> \<omega> B))"
     by (simp del:\<tau>.simps)
   also have "... = compress ( \<tau>\<^sub>3 \<omega> (A \<union> B) (max (s \<omega> A) (s \<omega> B)))"
-    by (subst merge1_result[OF assms]) blast 
+    by (subst merge1_result) blast 
   also have "... = ?R"
-    by (intro compress_result a)
+    by (intro compress_result a Un_least)
   finally show ?thesis by blast
 qed
-
 
 lemma single1_result: "single1 \<omega> x = \<tau>\<^sub>3 \<omega> {x} 0"
 proof -
@@ -356,10 +461,9 @@ proof -
   also have "... = compress ( \<tau>\<^sub>3 \<omega> {x} 0)"
     by (subst single1_result) blast
   also have "... = ?R"
-    by (intro compress_result) simp
+    by (intro compress_result) auto
   finally show ?thesis by blast
 qed
-
 
 
 end
--- Distributed_Distinct_Elements/Pseudorandom_Combinators.thy
+++ Distributed_Distinct_Elements/Pseudorandom_Combinators.thy
@@ -1,29 +1,192 @@
 theory Pseudorandom_Combinators
-  imports Main HOL.Rat
+  imports 
+    HOL.Rat 
+    "Finite_Fields.Card_Irreducible_Polynomials" 
+    "Universal_Hash_Families.Carter_Wegman_Hash_Family"
+    "Frequency_Moments.Product_PMF_Ext"
 begin
+
+definition GF :: "nat \<Rightarrow> int set list set ring"
+  where "GF n = (SOME F. finite_field F \<and> order F = n)"
+
+
+definition some_bij :: "'a set \<Rightarrow> (nat \<Rightarrow> 'a)"
+  where "some_bij A = (SOME f. inj_on f {..<card A} \<and> f ` {..<card A} = A \<and> f ` {card A..} = {f 0})"
+
+definition is_prime_power :: "nat \<Rightarrow> bool"
+  where "is_prime_power n \<longleftrightarrow> (\<exists>p k. Factorial_Ring.prime p \<and> k > 0 \<and> n = p^k)"
+definition split_prime_power :: "nat \<Rightarrow> (nat \<times> nat)"
+  where "split_prime_power n = (THE (p, k). p^k = n \<and> Factorial_Ring.prime p \<and> k > 0)" 
+
+lemma split_prime_power:
+  assumes "Factorial_Ring.prime p"
+  assumes "k > 0"
+  shows "split_prime_power (p^k) = (p,k)"
+proof -
+  have "q = p \<and> l = k" if "q^l = p^k" "Factorial_Ring.prime q" "l > 0" for q l
+  proof -
+    have "q dvd p^k" using that by (metis dvd_power)
+    hence "q dvd p" using prime_dvd_power that by auto
+    moreover have "p dvd q^l" using that assms(2) by (metis dvd_power)
+    hence "p dvd q" using prime_dvd_power that assms by blast
+    ultimately have a:"p = q" by auto
+    hence "l = k" using that prime_power_inj by auto
+    thus ?thesis using a by simp
+  qed
+  thus ?thesis
+    unfolding split_prime_power_def using assms
+    by (intro the_equality) auto
+qed
 
 record 'a sample_space = 
   size :: "nat"
   select :: "nat \<Rightarrow> 'a"
 
-definition triv_sample_space :: "nat \<Rightarrow> nat sample_space" 
-  where "triv_sample_space n = \<lparr> size = n, select = id \<rparr>"
+definition "sample_set S = (select S) ` {..<(size S)}"
+
+definition nat_sample_space :: "nat \<Rightarrow> nat sample_space" ("[_]\<^sub>S")
+  where "nat_sample_space n = \<lparr> size = n, select = id \<rparr>"
 
 definition prod_sample_space :: 
   "'a sample_space \<Rightarrow> 'b sample_space \<Rightarrow> ('a \<times> 'b) sample_space" (infixr "\<times>\<^sub>S" 65)
   where 
     "prod_sample_space s t = 
       \<lparr> size = size s * size t, 
-        select = (\<lambda>i. (select s (i mod (size s)), select t (i div (size s)))) \<rparr>"
+        select = (\<lambda>i. (select s (i mod (size s)), select t ((i div (size s)) mod (size t)))) \<rparr>"
 
-definition \<E> :: "'a sample_space \<Rightarrow> nat \<Rightarrow> rat \<Rightarrow> (nat \<Rightarrow> 'a) sample_space"
-  where "\<E> S l u = undefined"
+lemma prod_select:
+  assumes "size S > 0"
+  assumes "size T > 0" 
+  shows "select (S \<times>\<^sub>S T) x \<in> sample_set S \<times> sample_set T"
+  using assms
+  by (simp add:prod_sample_space_def sample_set_def)
 
-definition \<H> :: "nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> (nat \<Rightarrow> nat) sample_space"
-  where "\<H> k d r = undefined"
+lemma prod_sample_set:  "sample_set (S \<times>\<^sub>S T) \<subseteq> sample_set S \<times> sample_set T" 
+proof 
+  fix x
+  assume "x \<in> sample_set (S \<times>\<^sub>S T)"
+  then obtain j where j_def: "j < size (S \<times>\<^sub>S T)" and x_def:"x = select (S \<times>\<^sub>S T) j"
+    unfolding sample_set_def by auto
+  have "size (S \<times>\<^sub>S T) > 0" using j_def by simp
+  hence "size S > 0 \<and> size T > 0" 
+    unfolding prod_sample_space_def by simp
+  thus "x \<in> sample_set S \<times> sample_set T"
+    unfolding x_def  by (intro prod_select) auto
+qed
+
+definition \<E> :: "nat \<Rightarrow> rat \<Rightarrow> 'a sample_space \<Rightarrow> (nat \<Rightarrow> 'a) sample_space"
+  where "\<E> l \<Lambda> S = undefined"
+
+lemma \<E>_range:
+  assumes "size S > 0"
+  shows "select (\<E> l \<Lambda> S) i j \<in> sample_set S"
+  sorry
+
+definition \<H> :: "nat \<Rightarrow> nat \<Rightarrow> 'a sample_space \<Rightarrow> (nat \<Rightarrow> 'a) sample_space"
+  where "\<H> k d R = (
+    let (p,n) = split_prime_power (size R);
+        m = (LEAST j. d \<le> p^j \<and> j \<ge> n);
+        f = some_bij (carrier (GF (p^m)));
+        f' = the_inv_into {..<(p^m)} f;
+        g = some_bij (bounded_degree_polynomials (GF (p^m)) k) in
+    \<lparr> size = p^m, select = (\<lambda>i x. select R ((f' (ring.hash (GF (p^m)) (f i) (g i))) mod p^n))\<rparr>)"
+
+lemma 
+  fixes d :: nat
+  assumes "is_prime_power (size R)"
+  assumes "k > 0"
+  defines "S \<equiv> (\<H> k d R)"
+  shows 
+    \<H>_size: "size S > 0" and
+    \<H>_range: "range (select S i) \<subseteq> (sample_set R)" and
+(*    unif: "\<And>x. x < d \<Longrightarrow> y \<in> sample_set R \<Longrightarrow> \<P>(\<omega> in pmf_of_set {..< size S}. select S \<omega> x = y) = 1/(real (size R))" and
+    unif': "\<And>x. x < d \<Longrightarrow> map_pmf (\<lambda>\<omega>. select S \<omega> x) (pmf_of_set {..<size S}) = map_pmf (select R) (pmf_of_set {..<size R})" and *)
+    unif'': 
+      "\<And>D. D \<subseteq> {..<d} \<Longrightarrow> card D \<le> k \<Longrightarrow> map_pmf (\<lambda>\<omega>. (\<lambda>x \<in> D. select S \<omega> x)) (pmf_of_set {..<size S}) = 
+      map_pmf (\<lambda>\<omega>. (\<lambda>x \<in> D. select R (\<omega> x))) (prod_pmf D (\<lambda>_. pmf_of_set {..<size R}))" (is "\<And>D. ?A D \<Longrightarrow> ?B D \<Longrightarrow> ?C D") (* and   
+    indep: "prob_space.k_wise_indep_vars (pmf_of_set {..<size S}) k (\<lambda>_. discrete) (\<lambda>i \<omega>. select S \<omega> i) {..<d}" *)
+proof -
+  obtain p n where p_n_altdef: "n > 0" "Factorial_Ring.prime p" and size_R:"size R = p^n"
+    using assms(1) unfolding is_prime_power_def by auto
+  have p_n_def: "(p,n) = split_prime_power (size R)"
+    unfolding size_R
+    by (intro split_prime_power[symmetric] p_n_altdef)
+  define m where "m = (LEAST j. d \<le> p^j \<and> j \<ge> n)"
+  define f where "f = some_bij (carrier (GF (p^m)))"
+  define f' where "f' = the_inv_into {..<(p^m)} f"
+  define g where "g = some_bij (bounded_degree_polynomials (GF (p^m)) k)"
+
+  have p_n_gt_0: "p^n > 0" 
+    by (metis p_n_altdef(2) gr0I not_prime_0 power_not_zero)
+
+  have S_eq: "S = \<lparr> size = p^m, select = (\<lambda> i x. select R ((f' (ring.hash (GF (p^m)) (f i) (g i))) mod p^n )) \<rparr>"
+    unfolding S_def \<H>_def 
+    by (simp add:p_n_def[symmetric] m_def[symmetric] f_def[symmetric] g_def f'_def)
+
+  have "p^m > 0"
+    by (metis p_n_altdef(2) gr0I not_prime_0 power_not_zero)
+  thus "size S > 0" 
+    unfolding S_eq by simp
+
+  have "select S i x \<in> (sample_set R)" for x
+  proof -
+    have "f' (ring.hash (GF (p ^ m)) (f i) (g i)) mod p^n < p^n" using p_n_gt_0 by simp
+    hence "select R (f' ((ring.hash (GF (p ^ m)) (f i) (g i))) mod p^n) \<in> sample_set R" 
+      unfolding sample_set_def size_R by simp
+    thus ?thesis by (simp add:S_eq)
+  qed
+  thus "range (select S i) \<subseteq> (sample_set R)" by auto
+
+  show "\<And>D. ?A D \<Longrightarrow> ?B D \<Longrightarrow> ?C D" sorry
+qed
+
+fun count_zeros :: "nat \<Rightarrow> nat \<Rightarrow> nat" where 
+  "count_zeros 0 k = 0" |
+  "count_zeros (Suc n) k = (if odd k then 0 else 1 + count_zeros n (k div 2))"
+
+lemma count_zeros_max:
+  "count_zeros n k \<le> n"
+  by (induction n arbitrary: k) auto
+
+lemma is_prime_power: "Factorial_Ring.prime p \<Longrightarrow> k > 0 \<Longrightarrow> is_prime_power (p^k)" 
+  unfolding is_prime_power_def by auto
+
+definition \<G>' where 
+  "\<G>' n = \<lparr> size = 2^n, select = count_zeros n \<rparr>"
+
+lemma sample_set_\<G>': "sample_set (\<G>' n) \<subseteq> {..n}"
+  using count_zeros_max
+  unfolding sample_set_def \<G>'_def by auto
 
 definition \<G> :: "nat \<Rightarrow> nat \<Rightarrow> (nat \<Rightarrow> nat) sample_space"
-  where "\<G> k r = undefined"
+  where "\<G> k n = \<H> k n (\<G>' (max (nat \<lceil>log 2 n\<rceil>) 1))"
+
+lemma  
+  assumes "k > 0"
+  assumes "n > 0"
+  shows 
+    \<G>_range: "select (\<G> k n) i j \<le> max (nat \<lceil>log 2 n\<rceil>) 1" and
+    \<G>_size: "size (\<G> k n) > 0"
+proof -
+  let ?p = "max (nat \<lceil>log 2 n\<rceil>) 1"
+  let ?R = "\<G>' ?p"
+  have size_R: "size ?R = 2^?p" 
+    unfolding \<G>'_def by simp 
+  have a:"is_prime_power (size ?R)" 
+    unfolding size_R by (intro is_prime_power) auto 
+
+  hence "range (select (\<H> k n ?R) i) \<subseteq> sample_set ?R" 
+    by (intro \<H>_range assms(1)) auto
+  also have "... \<subseteq> {..?p}"
+    using sample_set_\<G>' by simp
+  finally have "range (select (\<H> k n ?R) i) \<subseteq> {..?p}" by simp
+  thus "select (\<G> k n) i j \<le> max (nat \<lceil>log 2 n\<rceil>) 1"
+    by (auto simp add:\<G>_def) 
+  show "size (\<G> k n) > 0"
+    unfolding \<G>_def by (intro \<H>_size a assms)
+qed
+
+
 
 
 end
